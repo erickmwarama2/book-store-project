8c0630488ce57eff90630b76409af3d7e6013324
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2025-05-20 21:26:31 +0300

    add views

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
+
+const rootDir = require("./utils/path");
 
 const app = express();
 
@@ -12,7 +15,8 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found </h1>");
+  // res.status(404).send("<h1>Page not found </h1>");
+  res.status(404).sendFile(path.join(rootDir, "views", "not-found.html"));
 });
 
 app.listen(3000, () => {
